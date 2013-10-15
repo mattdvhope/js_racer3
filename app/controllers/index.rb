28c9c60a75d0d01b932808e52d1215b4a96aa625
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-15 15:45:18 -0500

    updates

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,21 @@
 get '/' do
-  # Look in app/views/index.erb
+
   erb :index
 end
+
+post '/board' do
+  @player1 = Player.create(name: params[:player1])
+  @player2 = Player.create(name: params[:player2])
+  @game = Game.create(params[:id])
+  redirect "/board/#{@game.id}"
+end
+
+get '/board/:id' do
+  @game = Game.find(params[:id])
+  @player2 = Player.last
+  @player1 = Player.find(@player2.id - 1)
+  erb :board
+end
+
+
+
